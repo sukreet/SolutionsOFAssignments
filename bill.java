72e6b422a4c8d75a4b56da3dbda5cd4d2845b469
sukreet <sukreetr@thoughtworks.com>
2015-05-30 16:40:13 +0530

    programe7

---
+++ bill.java
@@ -0,0 +1,58 @@
+
+import java.io.* ;
+import java.util.Scanner;
+
+
+public class bill{
+
+
+public static int input()
+{
+	int a;
+	Scanner in = new Scanner(System.in);
+	System.out.println("Enter number");
+    a=in.nextInt();
+	return(a);
+}
+public static void triangle(int n)
+{
+				for(int i=1;i<=n;i++)
+				{
+					for(int k=n-i; k>0; k--)
+					System.out.print(" ");
+					
+					for(int j=1;j<=2*i-1;j++)
+					System.out.print("*");
+				System.out.println("");
+			    }
+}
+
+public static void reversetriangle(int n)
+{
+				for(int i=n;i>=1;i--)
+				{
+					for(int k=n-i; k>0; k--)
+					System.out.print(" ");
+					
+					for(int j=1;j<=2*i-1;j++)
+					System.out.print("*");
+				System.out.println("");
+			    }
+}	
+	public static void main(String[] args) 
+	{
+		int num;
+		num=input();
+        triangle(num-1);
+		System.out.println("BILL");
+		reversetriangle(num-1);
+
+    }
+
+}
+
+
+
+
+
+
